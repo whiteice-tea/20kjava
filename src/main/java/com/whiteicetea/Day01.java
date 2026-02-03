e0fc4a4354a8db03b788a8dbcfc3298b82e3739f
whiteice-tea <whiteicetea4@gmail.com>
2026-02-03 23:20:20 +0800

    Day02

---
+++ src/main/java/com/whiteicetea/Day01.java
@@ -0,0 +1,29 @@
+package com.whiteicetea;
+
+public class Day01 {
+    public static void main(String[] args){
+        System.out.println("Liu is studing java");
+        //  \t: 一个制表位，实现对齐的功能
+        System.out.println("北京\t天津\t上海");
+        //  \n: 换行符
+        System.out.println("jack\nxingqiu\nchongyun");
+        //  \\:一个\
+        System.out.println("2026\\2\\2");
+        //  \":一个“
+        System.out.println("你是 \"涅卡奇朵\"");
+        //  \':一个'
+        System.out.println("w\'o\'w");
+        //  \r:一个回车
+        System.out.println("我去，\r是回车啊");
+
+
+
+        System.out.println("书名\t作者\t价格\t销量\n三国演义\t罗贯中\t120\t1000");
+
+    }
+}
+//每一个类编译后，都对应一个.class
+//一个源文件中只能有一个public类。其他类的个数不限
+class Day01DOG{
+
+}
---
+++ src/main/java/com/whiteicetea/Day02.java
@@ -0,0 +1,8 @@
+package com.whiteicetea;
+
+public class Day02 {
+    public static void main(String[] args){
+        System.out.println("white\nman\nhubei\nliyue");
+
+    }
+}
