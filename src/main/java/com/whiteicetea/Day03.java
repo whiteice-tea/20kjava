9b182c3d12cceb8d853503d92ee49a5c9460c71b
whiteice-tea <whiteicetea4@gmail.com>
2026-02-05 22:38:33 +0800

    明天要更多

---
+++ src/main/java/com/whiteicetea/Day03.java
@@ -0,0 +1,16 @@
+package com.whiteicetea;
+
+public class Day03 {
+    public static void main(String[] args){
+        int a=60;
+        System.out.println(a);
+        double score = 88.6;
+        char gender = '男';
+        String name = "李华";
+
+        System.out.println(score);
+        System.out.println(gender);
+        System.out.println(name);
+
+    }
+}
